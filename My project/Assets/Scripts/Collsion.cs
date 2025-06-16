585efa40c17e5980d3cf6a29f5557b15798570f4
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2025-06-17 00:48:39 +0600

    19 Done

---
+++ My project/Assets/Scripts/Collsion.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Collsion : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision detected with: " + collision.gameObject.name);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger entered with: " + collision.gameObject.name);
+    }
+}
