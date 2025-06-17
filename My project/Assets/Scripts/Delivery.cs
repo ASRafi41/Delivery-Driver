85e2bebda8708e664a37235d8638c9ef9ec6b792
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2025-06-17 19:32:26 +0600

    24 Done

--- My project/Assets/Scripts/Delivery.cs
+++ My project/Assets/Scripts/Delivery.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision detected with: " + collision.gameObject.name);
@@ -10,13 +11,15 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         // Debug.Log("Trigger entered with: " + collision.gameObject.name);
-        if(collision.tag == "Package")
+        if(collision.tag == "Package" && !hasPackage)
         {
+            hasPackage = true;
             Debug.Log("Package delivered: " + collision.gameObject.name);
-            // Destroy(collision.gameObject); // Destroy the package on delivery
+             Destroy(collision.gameObject); // Destroy the package on delivery
         }
-        else if (collision.tag == "Customer")
+        else if (collision.tag == "Customer" && hasPackage)
         {
+            hasPackage = false;
             Debug.Log("Package delivered: " + collision.gameObject.name);
             // Destroy(collision.gameObject); // Destroy the package on delivery
         }
